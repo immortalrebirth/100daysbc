b7791590ca5bafebdf7113d5e6cd901c390d6da1
immortalrebirth <122955395+immortalrebirth@users.noreply.github.com>
2023-01-20 20:11:55 -0600

    Create bankerroulette.py

---
+++ day4/bankerroulette.py
@@ -0,0 +1,16 @@
+# Import the random module here
+import random
+# Split string method
+names_string = input("Give me everybody's names, separated by a comma. ")
+names = names_string.split(", ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+print(names)
+randomrange = len(names)
+randomrange -= 1
+#cannot use random as a variable, int, it would mess up something as it is the same name as the imported module
+randomperson = random.randint(0,randomrange)
+print(f"{names[randomperson]} is going to buy the meal today!")
+personpaying = random.choice(names)
+print(personpaying)
