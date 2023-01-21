e26f3d71c4acddca5f6c2e5f6e079fcfa1551c76
immortalrebirth <122955395+immortalrebirth@users.noreply.github.com>
2023-01-20 19:55:49 -0600

    Update bankerroulette.py

--- day3/bankerroulette.py
+++ day3/bankerroulette.py
@@ -12,5 +12,6 @@ randomrange -= 1
 #cannot use random as a variable, int, it would mess up something as it is the same name as the imported module
 randomperson = random.randint(0,randomrange)
 print(f"{names[randomperson]} is going to buy the meal today!")
+#below uses random.choice() to randomize name from list instead of using random integer
 personpaying = random.choice(names)
 print(personpaying)
